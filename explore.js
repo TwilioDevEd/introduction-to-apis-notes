b5ef1007d2eae363db6da19f529206ec7c29527d
Rachel Collins <spiritedndiligent@gmail.com>
2023-03-23 00:10:07 -0400

    client side calls

---
+++ explore.js
@@ -0,0 +1,9 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages.list({limit: 20})
+               .then(messages => messages.forEach(m => console.log(m.sid)));
